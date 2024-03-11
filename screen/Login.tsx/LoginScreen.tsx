e175b9d900697b0c2c9c3b4b7886bfd9f618ce0d
NguyenPhong <phongnguyenphong267@gmail.com>
2024-03-11 17:26:19 +0700

    update

--- screen/Login.tsx/LoginScreen.tsx
+++ screen/Login.tsx/LoginScreen.tsx
@@ -2,7 +2,59 @@ import React, {useEffect, useState} from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import {LoginButton, AccessToken} from 'react-native-fbsdk-next';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const storeDataString = async (value: any) => {
+  try {
+    await AsyncStorage.setItem('my-key', value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const storeDataObject = async (value: any) => {
+  try {
+    var JSONValue = JSON.stringify(value);
+    await AsyncStorage.setItem('my-object', JSONValue);
+  } catch (error) {
+    console.log('error:', error);
+  }
+};
+const readData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('my-key');
+    if (value !== null) {
+      console.log('value read data string:', value);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const readDataObject = async () => {
+  try {
+    const valueObject = await AsyncStorage.getItem('my-object');
+    //JSON.parse chuyển JSON thành javascript và JSOn.stringify chuyển JSOn thành object js
+    return valueObject !== null ? JSON.parse(valueObject) : null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const removeItem = async () => {
+  try {
+    await AsyncStorage.removeItem('my-object');
+  } catch (error) {
+    console.log(error);
+  }
+  console.log('done');
+};
+const getAllKeys = async () => {
+  let keys = [];
+  try {
+    keys = await AsyncStorage.getAllKeys();
+  } catch (error) {
+    console.log(error);
+  }
+  console.log('Done');
+};
 export default function Login() {
   const [userInfo, setUserInfo] = useState();
 
